9641369292397bf4603803f7c21f3f1ea98accd3
Hokyung (Andy) Lee <techandy42@gmail.com>
2022-03-18 03:06:02 -0400

    UserPage component finished

---
+++ src/components/user/UserPage.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { usersGetByUserName } from '../../../crudFunctions/users/usersRequests'
+import { useAppSelector } from '../../redux/app/hooks'
+import { selectAccountInfo } from '../../redux/features/accountInfoSlice'
+import IUser from '../../../interfaces/schema/IUser'
+import initialUserData from '../../../initialData/schema/initialUser'
+import UserAccount from '../../components/user/UserAccount'
+import { PageType, PageWidth } from '../../../enums/PageType'
+import TextPage from '../../components/miscellaneous/TextPage'
+import emptyAddress from '../../../constants/emptyAddress'
+
+interface IProps {
+  id: string | string[] | undefined
+  pageType: PageType
+}
+
+const userPage: React.FC<IProps> = ({ id, pageType }) => {
+  // fetch user info
+  const accountInfo = useAppSelector(selectAccountInfo)
+  const router = useRouter()
+
+  const [userId, setUserId] = useState<null | string>(null)
+  const [userData, setUserData] = useState<null | IUser>(initialUserData)
+
+  console.log('id: ', id)
+  console.log('userData: ', userData)
+
+  useEffect(() => {
+    if (
+      userData?.address !== emptyAddress &&
+      accountInfo?.account !== emptyAddress &&
+      userData?.address === accountInfo?.account
+    ) {
+      // if it is the current user's page
+      // redirect to their account
+      console.log('%ccurrent user', 'color:red;font-size:2.5rem')
+      router.push('/account')
+    }
+  }, [userData, accountInfo])
+
+  useEffect(() => {
+    const getUser = async (id: string) => {
+      const fetchedUser = await usersGetByUserName(id)
+      const fetchedUserData = fetchedUser && fetchedUser.data
+      setUserData(fetchedUserData)
+    }
+
+    console.log('this is called')
+    if (typeof id === 'string') {
+      // valid id
+      getUser(id)
+      setUserId(id)
+    } else if (id === undefined) {
+      // invalid id
+      // handle if id doesn't exist
+      setUserData(null)
+    } else {
+      // valid id
+      getUser(id[0])
+      setUserId(id[0])
+    }
+  }, [id])
+
+  if (userData === null) {
+    // if user info does not exist
+    return (
+      <TextPage
+        pageWidth={PageWidth.WIDE}
+        hasBackButton={true}
+        message='User Info does not exist'
+      />
+    )
+  } else if (userData.address === emptyAddress) {
+    // if user info is loading
+    return (
+      <TextPage
+        pageWidth={PageWidth.WIDE}
+        hasBackButton={false}
+        message='User Info is loading...'
+      />
+    )
+  } else {
+    // if user page is not the current user's
+    return <UserAccount pageType={pageType} />
+  }
+}
+
+export default userPage
--- src/pages/user/[id]/index.tsx
+++ src/pages/user/[id]/index.tsx
@@ -1,6 +1,3 @@
-/* Server-side Rendering Example */
-/* Modify in the future */
-
 /*
  * Todos:
  * reroute current user to the account page and its associated pages
@@ -10,7 +7,6 @@
 
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import Link from 'next/link'
 import { usersGetByUserName } from '../../../../crudFunctions/users/usersRequests'
 import { useAppSelector } from '../../../redux/app/hooks'
 import { selectAccountInfo } from '../../../redux/features/accountInfoSlice'
@@ -20,82 +16,14 @@ import UserAccount from '../../../components/user/UserAccount'
 import { PageType, PageWidth } from '../../../../enums/PageType'
 import TextPage from '../../../components/miscellaneous/TextPage'
 import emptyAddress from '../../../../constants/emptyAddress'
-
-interface IProps {
-  article: any
-}
+import UserPage from '../../../components/user/UserPage'
 
 const userPage = () => {
-  // fetch user info
-  const accountInfo = useAppSelector(selectAccountInfo)
   const router = useRouter()
-
-  const [userId, setUserId] = useState<null | string>(null)
-  const [userData, setUserData] = useState<null | IUser>(initialUserData)
-
+  // fetch page id
   const { id } = router.query
 
-  console.log('id: ', id)
-  console.log('userData: ', userData)
-
-  useEffect(() => {
-    if (
-      userData?.address !== emptyAddress &&
-      accountInfo?.account !== emptyAddress &&
-      userData?.address === accountInfo?.account
-    ) {
-      // if it is the current user's page
-      // redirect to their account
-      console.log('%ccurrent user', 'color:red;font-size:2.5rem')
-      router.push('/account')
-    }
-  }, [userData, accountInfo])
-
-  useEffect(() => {
-    const getUser = async (id: string) => {
-      const fetchedUser = await usersGetByUserName(id)
-      const fetchedUserData = fetchedUser && fetchedUser.data
-      setUserData(fetchedUserData)
-    }
-
-    console.log('this is called')
-    if (typeof id === 'string') {
-      // valid id
-      getUser(id)
-      setUserId(id)
-    } else if (id === undefined) {
-      // invalid id
-      // handle if id doesn't exist
-      setUserData(null)
-    } else {
-      // valid id
-      getUser(id[0])
-      setUserId(id[0])
-    }
-  }, [id])
-
-  if (userData === null) {
-    // if user info does not exist
-    return (
-      <TextPage
-        pageWidth={PageWidth.WIDE}
-        hasBackButton={true}
-        message='User Info does not exist'
-      />
-    )
-  } else if (userData.address === emptyAddress) {
-    // if user info is loading
-    return (
-      <TextPage
-        pageWidth={PageWidth.WIDE}
-        hasBackButton={false}
-        message='User Info is loading...'
-      />
-    )
-  } else {
-    // if user page is not the current user's
-    return <UserAccount pageType={PageType.ALL} />
-  }
+  return <UserPage id={id} pageType={PageType.ALL} />
 }
 
 export default userPage
