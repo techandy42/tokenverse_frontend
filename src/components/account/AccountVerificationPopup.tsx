12dd7ffad14724f017debf5a229447a54d763027
Hokyung (Andy) Lee <techandy42@gmail.com>
2022-04-22 22:16:31 -0400

    fixed dateFormat type error

--- src/components/account/AccountVerificationPopup.tsx
+++ src/components/account/AccountVerificationPopup.tsx
@@ -1,10 +1,5 @@
 import React, { useState } from 'react'
 import Typography from '@mui/material/Typography'
-import dateFormat from '../../../types/dateFormat'
-import WebAssetIcon from '@mui/icons-material/WebAsset'
-import FlexBox from '../styles/FlexBox'
-import WebsiteLink from './AccountInfo/WebsiteLink'
-import Box from '@mui/material/Box'
 import styles from './accountVerificationPopup/popup.module.css'
 
 interface IProps {
@@ -18,11 +13,6 @@ const AccountVerificationPopup: React.FC<IProps> = ({
   verificationDate,
   handleClose,
 }) => {
-  const formattedVerificationDate = dateFormat(
-    verificationDate,
-    'YYYY-MM-DD hh:mm:ss.SSS A',
-  )
-
   return (
     <div className={styles.popupbox}>
       <div className={styles.box}>
@@ -40,7 +30,7 @@ const AccountVerificationPopup: React.FC<IProps> = ({
           Verified User: {userName}
         </Typography>
         <Typography sx={{ marginBottom: '1rem' }}>
-          Verified Date: {formattedVerificationDate}
+          Verified Date: {verificationDate.toDateString()}
         </Typography>
         <Typography>
           Description: The user has been verified to be a trustworthy seller on
